fb6d38763436275da20efb7a6a5a1fd216509385
GigaTortilla <77364063+GigaTortilla@users.noreply.github.com>
2021-08-07 00:08:54 +0200

    Add files via upload

--- gfxtest/include/belegGFXConst.h
+++ gfxtest/include/belegGFXConst.h
@@ -1,22 +1,22 @@
 #define YELLOW_TOP 0xF794
 #define GREY_BG 0xA514
 
-// File generated 14.07.2021 00:13:02
+// File generated 05.08.2021 17:53:31
 // Warning! This is a generated file, any manual changes will be
 // lost during the next generation.
 
 #define  IFONT_OFFSET     0
 // object indexes into ImageControl
 #define iWinbutton1             0 // offset 0x0
-#define iThermometer1           1 // offset 0x3000
-#define iWinbutton2             2 // offset 0x171000
-#define iStatictext1            3 // offset 0x174400
-#define iWinbutton9             4 // offset 0x175E00
-#define iWinbutton10            5 // offset 0x17C200
-#define iWinbutton11            6 // offset 0x182600
-#define iWinbutton12            7 // offset 0x188A00
-#define iWinbutton13            8 // offset 0x18EE00
-#define iWinbutton14            9 // offset 0x195200
+#define iThermometer1           1 // offset 0x2400
+#define iWinbutton2             2 // offset 0x170400
+#define iStatictext1            3 // offset 0x173800
+#define iWinbutton9             4 // offset 0x175400
+#define iWinbutton10            5 // offset 0x17B800
+#define iWinbutton11            6 // offset 0x181C00
+#define iWinbutton12            7 // offset 0x188000
+#define iWinbutton13            8 // offset 0x18E400
+#define iWinbutton14            9 // offset 0x194800
 
 
 static const bool inputs[] PROGMEM = { true, false, true, false, true, true, true, true, 
